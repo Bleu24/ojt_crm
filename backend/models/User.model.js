5f8da49f9ab7da92dbaa86f54094295ec3ed4e84
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-07-07 14:31:39 +0800

    feat: Remove 'branch_manager' role from user schema

--- backend/models/User.model.js
+++ backend/models/User.model.js
@@ -13,7 +13,7 @@ const userSchema = new mongoose.Schema({
   },
   role: {
     type: String,
-    enum: ['intern', 'staff', 'unit_manager', 'branch_manager', 'admin'],
+    enum: ['intern', 'staff', 'unit_manager', 'admin'],
     default: 'intern',
   },
   supervisorId: {
